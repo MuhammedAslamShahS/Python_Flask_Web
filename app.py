390d9df40bea348bef6c4cb1ecc929e8e08b6091
MuhammedAslamShahS <muhammedaslamshahsofficial@gmail.com>
2025-02-01 05:17:00 -0800

    page routeing set

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
 from flask import Flask
 app = Flask(__name__)
 
+
+# Mian Page / Home page
+
 @app.route('/')
 def home():
-    return "Hello world"
+    return "Hello world \n This is Home page"
+
+
+# About page
+
+@app.route('/about')
+def about():
+    return "This is About Page"
+
+
+# Product page
+
+@app.route('/product')
+def product():
+    return "This is Product Page"
+
+
+
+# Product/laptop page
+
+@app.route('/product/laptop')
+def laptop():
+    return "This is Product/laptop Page"
+
+
+# Contact page
+
+@app.route('/contact')
+def contact():
+    return "This is Contact Page"
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True) 
